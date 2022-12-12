e97cc9f119cd7c916e059b59b3bde0ee0a27c75b
youssefjn <youssefjneyah@gmail.com>
2022-12-12 16:33:34 +0100

    added cross origins

--- Spring-Boot-Security-JWT/src/main/java/spring/security/jwt/controllers/TestController.java
+++ Spring-Boot-Security-JWT/src/main/java/spring/security/jwt/controllers/TestController.java
@@ -1,10 +1,11 @@
 package spring.security.jwt.controllers;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin (origins = "*")
 @RestController
 @RequestMapping ("/api/test")
 public class TestController {
