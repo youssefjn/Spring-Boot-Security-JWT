cd79c945e777056786f24ebac13d3ccd79e02986
youssefjn <youssefjneyah@gmail.com>
2022-12-14 08:57:25 +0100

    removed unused imports

--- Spring-Boot-Security-JWT/src/main/java/spring/security/jwt/repository/RoleRepository.java
+++ Spring-Boot-Security-JWT/src/main/java/spring/security/jwt/repository/RoleRepository.java
@@ -3,12 +3,10 @@ package spring.security.jwt.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import spring.security.jwt.models.ERole;
 import spring.security.jwt.models.Role;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
   Optional<Role> findByName(ERole name);
 }
